6afe0d3a0060c897776a766d782031b948a170dc
Kuba <j.sirkowski@gmail.com>
2024-02-12 17:34:51 +0000

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Jakub Sirkowski on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
